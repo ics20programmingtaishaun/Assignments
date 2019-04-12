983a1284fb1b92c5a3f94ea38efa4cef0181facc
TaishaunJ <47784239+TaishaunJ@users.noreply.github.com>
2019-04-12 09:17:21 -0400

    push

--- CompanyAnimationTaishaunJ/main.lua
+++ CompanyAnimationTaishaunJ/main.lua
@@ -1,7 +1,42 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Commpany Logo Animation
+-- Name: Taishaun Johnson
+-- Course: ICS2O/3C
+-- This program...
+----------------------------------------------------------------------------------------------------
 
--- Your code here
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- set the background colour
+display.setDefault("background", 28/255, 121/255, 243/255)
+
+-----------------------------------------------------------------------------------------------------------------
+-- Local Variables
+-----------------------------------------------------------------------------------------------------------------
+
+local logo 
+local scrollXSpeed = 8
+local scrollYSpeed = -3
+local sounds
+local soundsChannel
+
+-----------------------------------------------------------------------------------------------------------------
+
+-- display image 
+logo = display.newImageRect("images/CompanyLogoTaishaunJ@2x-Recovered-Recovered.png", 300, 500)
+logo.x = 100
+logo.y = 700
+
+-----------------------------------------------------------------------------------------------------------------
+-- Local Functions
+-----------------------------------------------------------------------------------------------------------------
+
+-- The function that moves the logo across the screen
+local function movelogo()
+	logo.x = logo.x + scrollXSpeed
+	logo.y = logo.y + scrollYSpeed
+end
